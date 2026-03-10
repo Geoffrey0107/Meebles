6e5a74187423b2734b96b6accc9ee5ef6843e448
Damien <damien8@comcast.net>
2026-03-10 12:40:56 -0400

    added countdown timer on main_activity screen

--- app/src/main/java/MAD/Meebles/MainActivity.java
+++ app/src/main/java/MAD/Meebles/MainActivity.java
@@ -2,9 +2,11 @@ package MAD.Meebles;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,7 +14,12 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.Calendar;
+
 public class MainActivity extends AppCompatActivity {
+    private TextView countdownText;
+
+    final int[][] targetTimes = { {12, 30, 0}, {18, 0, 0} }; // 12:00:00 and 18:00:00
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,7 +32,56 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
 
-        System.out.println("Hello World");
+        countdownText = (TextView) findViewById(R.id.timeDisplay);
+
+        startNextCountdown();
+    }
+
+    public void startNextCountdown() {
+        Calendar now = Calendar.getInstance();
+        Calendar nextTarget = null;
+
+        for (int[] t : targetTimes) {
+            Calendar temp = (Calendar) now.clone();
+            temp.set(Calendar.HOUR_OF_DAY, t[0]);
+            temp.set(Calendar.MINUTE, t[1]);
+            temp.set(Calendar.SECOND, t[2]);
+            temp.set(Calendar.MILLISECOND, 0);
+
+            if (temp.after(now)) {
+                nextTarget = temp;
+                break;
+            }
+        }
+
+        if (nextTarget == null) { // all times passed today, pick first target tomorrow
+            nextTarget = (Calendar) now.clone();
+            nextTarget.add(Calendar.DAY_OF_MONTH, 1);
+            nextTarget.set(Calendar.HOUR_OF_DAY, targetTimes[0][0]);
+            nextTarget.set(Calendar.MINUTE, targetTimes[0][1]);
+            nextTarget.set(Calendar.SECOND, targetTimes[0][2]);
+            nextTarget.set(Calendar.MILLISECOND, 0);
+        }
+
+        long diffMillis = nextTarget.getTimeInMillis() - now.getTimeInMillis();
+
+        new CountDownTimer(diffMillis, 1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                long seconds = millisUntilFinished / 1000 % 60;
+                long minutes = millisUntilFinished / (1000 * 60) % 60;
+                long hours   = millisUntilFinished / (1000 * 60 * 60);
+
+                countdownText.setText(String.format("%02d:%02d:%02d", hours, minutes, seconds));
+            }
+
+            @Override
+            public void onFinish() {
+                countdownText.setText("00:00:00");
+                // Start countdown to the next target automatically
+                startNextCountdown();
+            }
+        }.start();
     }
 
     public boolean onCreateOptionsMenu(Menu menu) {
